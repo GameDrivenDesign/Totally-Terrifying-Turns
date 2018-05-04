1cfffa95d0d3eed34cfb4a437dea9d3fc75eac30
Alexander Riese <alexander.riese@student.hpi.de>
2018-05-04 19:11:06 +0200

    Add light cone to player
    Co-authored-by: Eva <eva.krebs@student.hpi.de>

--- characters/player.gd
+++ characters/player.gd
@@ -11,14 +11,18 @@ func _ready():
 
 func _process(delta):
 	if(Input.is_action_pressed("move_left")):
+		rotation_degrees = 180
 		move_and_collide(Vector2(-1, 0))
 	elif(Input.is_action_pressed("move_right")):
+		rotation_degrees = 0
 		move_and_collide(Vector2(1, 0))
 	elif(Input.is_action_pressed("move_down")):
+		rotation_degrees = 90
 		move_and_collide(Vector2(0, 1))
 	elif(Input.is_action_pressed("move_up")):
+		rotation_degrees = 270
 		move_and_collide(Vector2(0, -1))
 	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
-
+	
