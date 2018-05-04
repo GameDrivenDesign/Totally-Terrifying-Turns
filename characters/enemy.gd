c01589cc502a38e211aa527e0646c3e51bf09c98
Sebastian Schulz <sebastian.schulz95@gmail.com>
2018-05-04 19:47:21 +0200

    make enemy path following respect the path length

--- characters/enemy.gd
+++ characters/enemy.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 
+const PATH_SPEED = 20.0
+const FOLLOW_SPEED = 50.0
+
 var direction = 1.0
-var speed = 0.2
+var path_length
 
 var followed_player_path = "../../../player"
 var followed_player = null
@@ -10,18 +13,21 @@ var time_to_follow = 0.0
 func _ready():
 	set_physics_process(true)
 	
+	path_length = get_parent().get_parent().curve.get_baked_length()
+	
 	followed_player = get_node(followed_player_path)
 
 	time_to_follow = 20.0
 
 
 func _physics_process(delta):
-	
+	#print(followed_player.position)
+	#move_and_collide((followed_player.global_position - global_position).normalized() * FOLLOW_SPEED * delta)
 	
 	#return
-	var unit_offset = get_parent().unit_offset
-	unit_offset = unit_offset + direction * speed * delta
-	if unit_offset < 0 or unit_offset > 1:
+	var offset = get_parent().offset
+	offset = offset + direction * PATH_SPEED * delta
+	if offset < 0 or offset > path_length:
 		direction = -direction
-		unit_offset = clamp(unit_offset, 0, 1)
-	get_parent().unit_offset = unit_offset
+		offset = clamp(offset, 0, path_length)
+	get_parent().offset = offset
