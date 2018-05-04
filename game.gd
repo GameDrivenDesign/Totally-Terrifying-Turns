26ae9e471447512620a8e33631c2ad065d27a037
Christian Flach <cmfcmf.flach@gmail.com>
2018-05-04 17:52:38 +0200

    Disable game maximation :( Can't get rid of Memory Exception

--- game.gd
+++ game.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 func _ready():
-	if OS.has_feature('JavaScript'):
-		# Maximize game in browsers
-		OS.set_window_maximized(true)
+	# TODO: Doesn't seem to work, throws out of memory exception.
+	#if OS.has_feature('JavaScript'):
+	#	# Maximize game in browsers
+	#	OS.set_window_maximized(true)
+	pass
