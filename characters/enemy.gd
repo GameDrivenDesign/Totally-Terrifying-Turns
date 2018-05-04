2a9ad8bffbae8da00e961c66b0c77a3dd32d4488
Sebastian Schulz <sebastian.schulz95@gmail.com>
2018-05-04 18:59:30 +0200

    cleaned up enemy

--- characters/enemy.gd
+++ characters/enemy.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends PathFollow2D
 
 var direction = 1.0
 var speed = 0.2
@@ -7,13 +7,7 @@ func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
-	var unit_offset = get_parent().unit_offset
-	
 	unit_offset = unit_offset + direction * speed * delta
 	if unit_offset < 0 or unit_offset > 1:
 		direction = -direction
 		unit_offset = clamp(unit_offset, 0, 1)
-		
-	get_parent().unit_offset = unit_offset
-	
-	#get_parent().set_offset(get_parent().get_offset() + (50*delta))
