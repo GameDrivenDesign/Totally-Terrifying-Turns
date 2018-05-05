1efe58e860567489c0bd94f6a4cccff2d0152673
Sebastian Schulz <sebastian.schulz95@gmail.com>
2018-05-05 11:09:43 +0200

    make common enemy functions static

--- characters/enemies/common.gd
+++ characters/enemies/common.gd
@@ -1,10 +1,10 @@
 extends Node
 
-func calc_angle(vec2):
+static func calc_angle(vec2):
 	if vec2.x < 0:
 		return -acos(Vector2(0.0, -1.0).dot(vec2))
 	else:
 		return acos(Vector2(0.0, -1.0).dot(vec2))
 	
-func calc_direction(enemy, player):
+static func calc_direction(enemy, player):
 	return (player.global_position - enemy.global_position).normalized()
