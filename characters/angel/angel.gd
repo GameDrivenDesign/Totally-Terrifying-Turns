290b7db7069837f794898fc9af227ce95326f6ad
Sebastian Schulz <sebastian.schulz95@gmail.com>
2018-05-05 10:23:10 +0200

    add rotation to angels

--- characters/angel/angel.gd
+++ characters/angel/angel.gd
@@ -12,6 +12,12 @@ var enemy_state = ENEMY_STATE.inactive
 
 var followed_player = null
 
+func calc_angle(vec2):
+	return acos(Vector2(0.0, -1.0).dot(vec2))
+	
+func calc_direction(player):
+	return (followed_player.global_position - global_position).normalized()
+
 func _ready():
 	set_physics_process(true)
 
@@ -33,4 +39,7 @@ func on_exited_neighborhood(player):
 
 func _physics_process(delta):
 	if enemy_state == ENEMY_STATE.following:
-		move_and_collide((followed_player.global_position - global_position).normalized() * FOLLOW_SPEED * delta)
+		var direction = calc_direction(followed_player)
+		print(calc_angle(direction))
+		move_and_collide(direction * FOLLOW_SPEED * delta)
+		rotation = calc_angle(direction)
