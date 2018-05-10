12920f3e01d7fe94a21ec96dd66df752da706754
Christian Flach <cmfcmf.flach@gmail.com>
2018-05-10 13:01:22 +0200

    Select a single music piece, simplify CanvasModulate code

--- CanvasModulate.gd
+++
@@ -1,6 +0,0 @@
-extends CanvasModulate
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	set_color(config.modulate_color())
--- game.gd
+++ game.gd
@@ -1,5 +1,8 @@
 extends "res://screen.gd"
 
+func _ready():
+	$CanvasModulate.set_color(config.modulate_color())
+
 func _on_player_lose():
 	start_gameoverscreen()
 
