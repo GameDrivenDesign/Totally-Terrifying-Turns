1cf6228596e77a21d8c16d101c380e07c7bb7a6c
Christian Flach <cmfcmf.flach@gmail.com>
2018-05-10 12:15:34 +0200

    Re-add player to debug_level, add level to start_game function

--- screen.gd
+++ screen.gd
@@ -1,23 +1,15 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
-
 # create a function to switch between scenes 
 func start_scene(scenePath):
-	var s = ResourceLoader.load(scenePath)
-	var currentScene = s.instance()
+	var currentScene = load(scenePath).instance()
 	get_tree().get_root().add_child(currentScene)
 	self.queue_free()
+	return currentScene
 
-func start_game():
-	start_scene("res://game.tscn")
+func start_game(level):
+	var game_scene = start_scene("res://game.tscn")
+	game_scene.get_node("level_navigation").add_child(level)
 
 func start_intro():
 	start_scene("res://intro/intro.tscn")
