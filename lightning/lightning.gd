f0d751e6a1d3e1acdf63dd048bb4c50725360dc9
Christian Flach <cmfcmf.flach@gmail.com>
2018-05-10 14:29:57 +0200

    Use AnimationPlayer for lightning effect
    Using _process() was using a lot of CPU

--- lightning/lightning.gd
+++ lightning/lightning.gd
@@ -1,22 +1,11 @@
-extends Light2D
-
-const MAX_LIGHT_PROGRESS = 2.0
-
-var light_progress = 0.0
-
-func _ready():
-	set_process(true)
-	
-func _process(delta):
-	light_progress -= delta
-	light_progress = max(light_progress, 0.0)
-	energy = light_progress / MAX_LIGHT_PROGRESS
-	
-func thunder():
-	$timer.wait_time = randi()%11+4
-	$timer.start()
-	$audio_player.play()
-	light_progress = MAX_LIGHT_PROGRESS
+extends Node2D
 
 func _on_timer_timeout():
 	thunder()
+
+func thunder():
+	$audio_player.play()
+	$animation_player.play("lightning")
+	
+	$timer.wait_time = randi() % 11 + 4
+	$timer.start()
