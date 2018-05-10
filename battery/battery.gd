f3b042e6aec1036fbc5283458ef15f29362ee71f
Christian Flach <cmfcmf.flach@gmail.com>
2018-05-10 11:49:24 +0200

    Remove boilerplate code

--- battery/battery.gd
+++ battery/battery.gd
@@ -2,11 +2,6 @@ extends Sprite
 
 const CAPACITY = 0.5
 
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
-
 func _on_area_body_entered(body):
 	if body.is_in_group("players"):
 		body.recharge_battery(CAPACITY)
